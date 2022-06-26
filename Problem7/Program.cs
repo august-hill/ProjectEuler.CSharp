34c77618c6460cd54299b0888712476e8c9f18d1
august-hill <august.hill@live.com>
2022-06-25 17:09:45 -0700

    Problems 5, 6, 7, 8, 9

---
+++ Problem7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+// Problem7
+// 10001st prime
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//
+// What is the 10,001st prime number?
+
+
+namespace Problem7
+{
+    class Program
+    {
+        private static ulong Prime(int n)
+        {
+            // Generate a list of Prime numbers...
+            var primes = new List<ulong> {2};   // Special case 2, so we can increment by 2 below
+
+            // We're going to start generating Prime numbers
+            for (ulong i = 3L; primes.Count < n; i += 2L)
+            {
+                // Assume it is Prime...
+                bool valid = true;
+                foreach (ulong p in primes)
+                {
+                    // If i is evenly divisible by a Prime, 
+                    // then it's not Prime and we skip it.
+                    if (i % p == 0)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid)
+                {
+                    // Keep track of Primes we've seen.
+                    primes.Add(i);
+                }
+            }
+
+            return primes[^1];  // new syntax
+        }
+
+        private static void Main()
+        {
+            var timeTaken = Stopwatch.StartNew();
+            const int nthPrime = 10001;
+            var prime = Prime(nthPrime);
+            timeTaken.Stop();
+            Console.WriteLine("The {0} Prime is {1} in {2} ms.", nthPrime, prime, timeTaken.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Problem9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+// Problem9
+// Special Pythagorean triplet
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//
+// a2 + b2 = c2
+// For example, 32 + 42 = 9 + 16 = 25 = 52.
+//
+//     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//     Find the product abc.
+
+namespace Problem9
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var timeTaken = Stopwatch.StartNew();
+            // a < b < c
+            // a^2 + b^2 = c^2
+            // a + b + c = 1000
+
+            for (int a = 1; a < 998; a++)
+            {
+                for (int b = a + 1; b < 999; b++)
+                {
+                    for (int c = b + 1; c < 1000; c++)
+                    {
+                        if (a + b + c == 1000)
+                        {
+                            if (a * a + b * b == c * c)
+                            {
+                                Console.WriteLine($"a={a}, b={b}, c={c}, a+b+c={a+b+c}, abc={a * b * c}, in {timeTaken.ElapsedMilliseconds} ms.");
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
