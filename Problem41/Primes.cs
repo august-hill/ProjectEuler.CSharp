a9c1b5dd87406ea7e68dfc8db99723a81a3f26e8
august-hill <august.hill@live.com>
2022-06-25 17:28:11 -0700

    Problem 41

---
+++ Problem41/Primes.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+
+// Problem41
+// Pandigital prime
+// We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example,
+// 2143 is a 4-digit pandigital and is also prime.
+//
+// What is the largest n-digit pandigital prime that exists?
+
+namespace Problem41
+{
+    class Primes
+    {
+        private readonly BitArray _sieve;
+        public Primes(int limit)
+        {
+            // Sieve implemented without storing even numbers...
+            // Now prime p, is represented as p = 2 * i + 1, where i is sieve[i].
+
+            int sieveBound = (limit - 1) / 2;   // last index of the sieve
+            _sieve = new BitArray(sieveBound + 1);
+            int crossLimit = (Convert.ToInt32(Math.Floor(Math.Sqrt(Convert.ToDouble(limit)))) - 1) / 2;
+            for (int i = 1; i <= crossLimit; i++)
+            {
+                if (!_sieve[i])
+                {
+                    for (int j = 2 * i * (i + 1); j <= sieveBound; j += 2 * i + 1)
+                    {
+                        _sieve[j] = true;
+                    }
+                }
+            }
+        }
+
+        private bool IsPrime(int p)
+        {
+            // 1 is a unit, not composite or prime
+            if (p < 2)
+            {
+                return false;
+            }
+
+            // 2 is a prime
+            if (p == 2)
+            {
+                return true;
+            }
+
+            // even numbers are not prime
+            if (p % 2 == 0)
+            {
+                return false;
+            }
+
+            return !_sieve[(p - 1) / 2];
+        }
+
+        public bool IsComposite(int c)
+        {
+            // 1 is a unit, not composite or prime
+            if (c < 2)
+            {
+                return false;
+            }
+
+            return !IsPrime(c);
+        }
+
+        public List<int> GetPrimeList()
+        {
+            var primes = new List<int> {2};
+            for (int i = 1; i < _sieve.Length - 1; i++)
+            {
+                if (!_sieve[i])
+                {
+                    primes.Add(i * 2 + 1);
+                }
+            }
+
+            return primes;
+        }
+    }
+}
---
+++ Problem41/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace Problem41
+{
+    class Program
+    {
+        static bool IsPandigital(int i)
+        {
+            var flags = new BitArray(10)
+            {
+                [0] = true
+            };
+
+            int k = 1;
+            while (i != 0)
+            {
+                int digit = i % 10;
+                if (flags[digit])
+                {
+                    return false;
+                }
+                flags[digit] = true;
+                i = i / 10;
+                ++k;
+            }
+
+            for (int j = 1; j < k; ++j)
+            {
+                if (!flags[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void Main()
+        {
+            var timeTaken = Stopwatch.StartNew();
+            // Note: Nine numbers cannot be done (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = 45 = > always dividable by 3)
+            // Note: Eight numbers cannot be done (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36 = > always dividable by 3)
+
+            const int pandigitalMax = 7654321;
+            var p = new Primes(pandigitalMax);
+            var primes = p.GetPrimeList();
+
+            int result = 0;
+            foreach(var prime in primes)
+            {
+                if (IsPandigital(prime) && prime > result)
+                {
+                    result = prime;
+                }
+
+            }
+
+
+            timeTaken.Stop();
+            Console.WriteLine($"largest pandigital is {result} in {timeTaken.ElapsedMilliseconds} ms.");
+        }
+    }
+}
