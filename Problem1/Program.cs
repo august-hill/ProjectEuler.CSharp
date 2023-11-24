159ea1c480e900ba4aa56d35a4bc8fea4863a044
August Hill <august.hill@live.com>
2023-11-23 19:59:59 -0800

    Update target framework and optimize Problem9
    The target .NET framework has been updated from 6.0 to 7.0 in several project files and an additional using directive has been added to Problem1. Problem9's program logic has been significantly streamlined to improve execution time by removing unnecessary iterations. The language version for Problem1's project file has been explicitly set to 11.

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 //  Multiples of 3 and 5
 //  Problem 1
--- Problem9/Program.cs
+++ Problem9/Program.cs
@@ -4,41 +4,31 @@
 // Special Pythagorean triplet
 // A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 //
-// a2 + b2 = c2
-// For example, 32 + 42 = 9 + 16 = 25 = 52.
+// a^2 + b^2 = c^2
+// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 //
 //     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 //     Find the product abc.
 
 namespace Problem9
 {
-    class Program
+    internal static class Program
     {
         private static void Main()
         {
             var timeTaken = Stopwatch.StartNew();
-            // a < b < c
-            // a^2 + b^2 = c^2
-            // a + b + c = 1000
 
             for (int a = 1; a < 998; a++)
             {
                 for (int b = a + 1; b < 999; b++)
                 {
-                    for (int c = b + 1; c < 1000; c++)
-                    {
-                        if (a + b + c == 1000)
-                        {
-                            if (a * a + b * b == c * c)
-                            {
-                                Console.WriteLine($"a={a}, b={b}, c={c}, a+b+c={a+b+c}, abc={a * b * c}, in {timeTaken.ElapsedMilliseconds} ms.");
-                                return;
-                            }
-                        }
-                    }
+                    int c = 1000 - a - b;
+
+                    if (c <= 0 || a * a + b * b != c * c) continue;
+                    Console.WriteLine($"a={a}, b={b}, c={c}, a+b+c={a+b+c}, abc={a * b * c}, in {timeTaken.ElapsedTicks} ticks.");
+                    return;
                 }
             }
-
         }
     }
 }
